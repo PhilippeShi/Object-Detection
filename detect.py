862f85c4d27182def04dac7cc2e1516c75e2a406
PhilippeShi <77639323+PhilippeShi@users.noreply.github.com>
2021-12-27 22:14:09 -0500

    Update detect.py

--- detect.py
+++ detect.py
@@ -2,16 +2,16 @@ from cv2 import cv2
 import numpy as np
 
 # Load Yolo
-net = cv2.dnn.readNet("C:/Users/phili/OneDrive/Projects/OpenCV/yolov3.weights", "C:/Users/phili/OneDrive/Projects/OpenCV/yolov3.cfg")
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
 classes = []
-with open("C:/Users/phili/OneDrive/Projects/OpenCV/coco.names", "r") as f:
+with open("coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
 # Loading image
-img = cv2.imread("C:/Users/phili/OneDrive/Projects/OpenCV/office.jpg")
+img = cv2.imread("office.jpg")
 img = cv2.resize(img, None, fx=0.4, fy=0.4)
 height, width, channels = img.shape
 
